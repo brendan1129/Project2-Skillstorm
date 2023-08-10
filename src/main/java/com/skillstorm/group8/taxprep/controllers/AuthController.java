9cc2f65c1364830220fdce940739be53f14348da
jkersey99 <137439356+jkersey99@users.noreply.github.com>
2023-08-10 12:34:31 -0400

    starting spring security

---
+++ src/main/java/com/skillstorm/group8/taxprep/controllers/AuthController.java
@@ -0,0 +1,13 @@
+package com.skillstorm.group8.taxprep.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin("*")
+public class AuthController {
+
+
+}
